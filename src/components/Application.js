1c9ba3ff777d67f6ecbd85231e7ad91caddb696f
Nicole Ticknor <36491978+nicoleticknor@users.noreply.github.com>
2020-05-13 14:49:15 -0700

    added days API and helper function

--- src/components/Application.js
+++ src/components/Application.js
@@ -63,8 +63,14 @@ const appointments = [
 
 export default function Application(props) {
 
-  const [day, setDay] = useState('Monday');
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: 'Monday',
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
     axios
@@ -73,7 +79,7 @@ export default function Application(props) {
         setDays(r.data);
 
       })
-  }, [])
+  }, []);
 
   const appts = appointments.map(appt =>
     <Appointment key={appt.id} {...appt} />);
@@ -89,8 +95,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            day={day}
+            days={state.days}
+            day={state.day}
             handleSetDay={setDay}
           />
         </nav>
---
+++ src/helpers/selectors.js
@@ -0,0 +1,24 @@
+//these are called a selector because it accepts state as an argument and returns data derived from that state
+
+export function getAppointmentsForDay(state, day) {
+
+  const dayObj = state.days.find(d => d.name === day);
+  // console.log(dayObj);
+
+  //some validation for if the day has no appointments
+  if (dayObj === undefined) {
+    return [];
+  }
+
+  const appts = dayObj.appointments.reduce((acc, a) => {
+    for (const item in state.appointments) {
+      if (a === Number(item)) {
+        acc.push(state.appointments[item]);
+      }
+    }
+    return acc;
+  }, []);
+
+  return appts;
+
+}
