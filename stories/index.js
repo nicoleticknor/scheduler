9d54d691ac3987fdb657b43891ad16fd47a710ff
Nicole Ticknor <36491978+nicoleticknor@users.noreply.github.com>
2020-05-12 19:26:30 -0700

    adding header, show/empty to appointment

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 /****************** BUTTON *****************/
 
@@ -178,3 +179,35 @@ storiesOf("Appointment", module)
     message={"Could not delete appointment."}
     onClose={action("onClose")}
   />)
+  .add("Editing", () => (
+    <Form
+      student="Lydia Miller-Jones"
+      interviewers={interviewers}
+      val={3}
+      onSave={action("onSave")}
+      onCancel={action("onChange")}
+    />
+  ))
+  .add("Creating", () => (
+    <Form
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onChange")}
+    />
+  )).add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="12pm" />
+      <Appointment id="last" time="1pm" />
+    </Fragment>
+  ))
+  .add("Appointment Booked", () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="12pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+      />
+      <Appointment id="last" time="1pm" />
+    </Fragment>
+  ));
+
