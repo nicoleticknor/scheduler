ee9375fade7e4068d0b08bd901dc1db65a4246ca
Nicole Ticknor <36491978+nicoleticknor@users.noreply.github.com>
2020-05-13 13:25:14 -0700

    adding axios package

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
+import axios from 'axios';
 
 import "components/Application.scss";
 import DayList from "components/DayList/DayList";
 import InterviewerList from "components/InterviewerList/InterviewerList";
+import Appointment from "components/Appointment/index";
 
 const days = [
   {
@@ -22,9 +24,67 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "Steve Yzerman",
+      interviewer: {
+        id: 5,
+        name: "Sven Jones",
+        avatar: "https://i.imgur.com/twYrpay.jpg",
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "The Rubber Ducky",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      }
+    }
+  }
+];
+
 export default function Application(props) {
 
   const [day, setDay] = useState('Monday');
+
+  const appts = appointments.map(appt =>
+    <Appointment key={appt.id} {...appt} />);
 
   return (
     <main className="layout">
@@ -49,7 +109,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appts}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
