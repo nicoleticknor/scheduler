b7e65d96a6924bec0df82df3c265503d703f384a
Nicole Ticknor <36491978+nicoleticknor@users.noreply.github.com>
2020-05-15 17:44:33 -0700

    spots remaining calculation complete

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -3,15 +3,6 @@ import axios from 'axios';
 
 export default function useApplicationData() {
 
-  const [state, setState] = useState({
-    day: 'Monday',
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
-
-  const setDay = day => setState({ ...state, day });
-
   useEffect(() => {
     const p1 = axios.get('http://localhost:8001/api/days');
     const p2 = axios.get('http://localhost:8001/api/appointments');
@@ -22,7 +13,43 @@ export default function useApplicationData() {
           return { ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }
         })
       })
+      .catch(() => axios.get('http://localhost:8001/api/debug/reset'))
   }, []);
+
+  const setDay = day => setState({ ...state, day });
+
+  const [state, setState] = useState({
+    day: 'Monday',
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
+
+  function calculateSpots(appointments) {
+
+    const today = state.days.find(d => d.name === state.day);
+
+    const spotsRemaining = today.appointments.reduce((acc, d) => {
+      if (!appointments[d].interview) {
+        acc++;
+      }
+      return acc;
+    }, 0);
+
+    today.spots = spotsRemaining;
+
+    let index;
+    for (let i = 0; i < state.days.length; i++) {
+      if (state.days[i].name === today.name) {
+        index = i;
+      }
+    }
+
+    const days = [...state.days];
+    days[index] = today;
+
+    return days;
+  }
 
   function bookInterview(id, interview) {
     const appointment = {
@@ -33,11 +60,18 @@ export default function useApplicationData() {
       ...state.appointments,
       [id]: appointment
     };
+    const days = calculateSpots(appointments);
     return (
       axios
         .put(`http://localhost:8001/api/appointments/${id}`, { interview })
         .then(() => {
-          setState(state => { return { ...state, appointments: appointments } })
+          setState(state => {
+            return {
+              ...state,
+              appointments: appointments,
+              days
+            }
+          })
         })
     )
   }
@@ -51,11 +85,15 @@ export default function useApplicationData() {
       ...state.appointments,
       [id]: appointment
     };
+
+    const days = calculateSpots(appointments);
+
     return (
       axios
         .delete(`http://localhost:8001/api/appointments/${id}`)
         .then(() => {
-          setState(state => { return { ...state, appointments: appointments } })
+
+          setState(state => { return { ...state, appointments: appointments, days } })
         })
     )
   }
