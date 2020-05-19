6830144387842c8dc4040e6782e94c408e38c628
Nicole Ticknor <36491978+nicoleticknor@users.noreply.github.com>
2020-05-19 10:03:12 -0700

    fixed styling on daylistitem

--- src/components/DayListItem/DayListItem.js
+++ src/components/DayListItem/DayListItem.js
@@ -1,33 +1,28 @@
 import React from "react";
 
 import "components/DayListItem/DayListItem.scss";
-const classNames = require('classnames');
+const classnames = require('classnames');
 
 export default function DayListItem(props) {
-  let dayClass = "day-list"
-  let spotsRemaining = ''
 
-  if (props.selected) {
-    dayClass += "__item--selected";
-  }
+  //generate text for h3
+  let spotsRemaining = props.spots;
+  props.spots !== 1 ? spotsRemaining += ' spots remaining' :
+    spotsRemaining += ' spot remaining';
 
-  if (props.spots === 0) {
-    dayClass += "__item--full";
-    spotsRemaining = 'no spots remaining';
-  } else if (props.spots === 1) {
-    spotsRemaining = props.spots + ' spot remaining';
-  } else {
-    spotsRemaining = props.spots + ' spots remaining';
-  }
-
-  if (!props.selected && props.spots !== 0) {
-    dayClass += "__item";
-  }
+  //generate className
+  let full;
+  props.spots === 0 ? full = true : full = false;
+  const dayListClass = classnames({
+    'day-list__item': true,
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': full
+  })
 
   return (
-    <li className={dayClass} onClick={() => { props.setDay(props.name) }}>
-      <h2 className={dayClass}>{props.name}</h2>
-      <h3 className={dayClass}>{spotsRemaining}</h3>
+    <li className={dayListClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{spotsRemaining}</h3>
     </li>
   );
 }
