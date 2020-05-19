f5c54b80360793706d7b3e994fe314087d1a5e88
Nicole Ticknor <36491978+nicoleticknor@users.noreply.github.com>
2020-05-19 14:49:00 -0700

    starting mock axios testing

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,15 @@
 import React from "react";
-
-import { render, cleanup } from "@testing-library/react";
-
+import { render, cleanup, fireEvent, waitForElement } from "@testing-library/react";
 import Application from "components/Application";
+// import axios from "../../__mocks__/axios"
 
 afterEach(cleanup);
 
-xit("renders without crashing", () => {
-  render(<Application />);
+it("defaults to Monday and changes the schedule when a new day is selected", () => {
+  const { getByText } = render(<Application />);
+
+  return waitForElement(() => getByText("Monday")).then(() => {
+    fireEvent.click(getByText("Tuesday"));
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
 });
